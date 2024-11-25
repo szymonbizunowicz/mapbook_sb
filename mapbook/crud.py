f6b99e93529711666e7f545f5e27864e2e48fe21
szymonbizunowicz <szymon.bizunowicz@student.wat.edu.pl>
2024-11-25 14:33:01 +0100

    feat: add read functionalities

---
+++ mapbook/crud.py
@@ -0,0 +1,6 @@
+def hello(user:str)->None:
+    print(f'Witaj {user}!!')
+
+def read_users(users:list)->None:
+    for user in users[1:]:
+        print(f'Twój znajomy {user['name']}, z miejscowości {user['city']}, opublikował {user['posts']} postów')
---
+++ mapbook/users.py
@@ -0,0 +1,12 @@
+users: list = [
+    {'name': 'Szymon', 'posts': 1, 'city': 'Warszawa'},
+    {'name': 'Dominik', 'posts': 3, 'city': 'Poznań'},
+    {'name': 'Szymon z wąsem', 'posts': 5, 'city': 'Toruń'},
+    {'name': 'Patrycja', 'posts': 7, 'city': 'Łódź'},
+    {'name': 'Patryk', 'posts': 9, 'city': 'Kielce'},
+    {'name': 'Zerom', 'posts': 10, 'city': 'Katowice'},
+    {'name': 'Michal', 'posts': 11, 'city': 'Bytom'},
+    {'name': 'Dominik', 'posts': 12, 'city': 'Bydgoszcz'},
+    {'name': 'Kinga', 'posts': 13, 'city': 'Morąg'},
+    {'name': 'Amelia', 'posts': 20, 'city': 'Gdańsk'},
+]
