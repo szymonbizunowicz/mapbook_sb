8a8104bc0cd3268094517497d5ee7f69effd9d65
szymonbizunowicz <szymon.bizunowicz@student.wat.edu.pl>
2025-01-10 09:32:18 +0100

    feat: add user, remove user in tkinter

--- main.py
+++ main.py
@@ -15,19 +15,43 @@ class User:
 
 
 
-
-
-
 def main():
     users = [
-        User('aaa', 'aaa', '1', 'aaa'),
-        User('bbb', 'bbb', '2', 'bbb'),
-        User('ccc', 'ccc', '3', 'ccc'),
+        # User('aaa', 'aaa', '1', 'aaa'),
+        # User('bbb', 'bbb', '2', 'bbb'),
+        # User('ccc', 'ccc', '3', 'ccc'),
     ]
 
     def show_users():
+        listbox_lista_obiektow.delete(0, END)
         for idx, user in enumerate(users):
-            listbox_lista_obiektow.insert(idx, user.imie)
+            listbox_lista_obiektow.insert(idx, f'{user.imie} {user.nazwisko} {user.postow} {user.lokalizacja}')
+
+    def add_user()->None:
+        name = entry_imie.get()
+        surname = entry_nazwisko.get()
+        posts = entry_liczba_postow.get()
+        location = entry_lokalizacja.get()
+
+        new_user = User(name, surname, posts, location)
+
+        users.append(new_user)
+        show_users()
+        entry_imie.delete(0, END)
+        entry_nazwisko.delete(0, END)
+        entry_liczba_postow.delete(0, END)
+        entry_lokalizacja.delete(0, END)
+        entry_imie.focus()
+
+
+
+    def delete_user()->None:
+        i = listbox_lista_obiektow.index(ACTIVE)
+        print(i)
+        users.pop(i)
+        show_users()
+
+
 
     root = Tk()
     root.geometry("1100x800")
@@ -37,12 +61,11 @@ def main():
     ramka_lista_obiektow = Frame(root)
     ramka_folmularz = Frame(root)
     ramka_szczegoly_obiektu = Frame(root)
+    ramka_mapa = Frame(root)
 
     ramka_lista_obiektow.grid(row=0, column=0, padx=50)
     ramka_folmularz.grid(row=0, column=1)
     ramka_szczegoly_obiektu.grid(row=1, column=0, padx=50, pady=20, columnspan=2)
-
-    ramka_mapa = Frame(root)
     ramka_mapa.grid(row=2, column=0, padx=50, pady=20)
 
     # ramka lista obiektów
@@ -52,7 +75,7 @@ def main():
     listbox_lista_obiektow.grid(row=1, column=0, columnspan=3)
     button_pokaz_szczegoly = Button(ramka_lista_obiektow, text = "Pokaż szczegóły", command=lambda: print("AAA"))
     button_pokaz_szczegoly.grid(row=2, column=0)
-    button_usun_obiekt = Button(ramka_lista_obiektow, text = "Usuń obiekt")
+    button_usun_obiekt = Button(ramka_lista_obiektow, text = "Usuń obiekt", command = delete_user)
     button_usun_obiekt.grid(row=2, column=1)
     button_edytuj_obiekt = Button(ramka_lista_obiektow, text = "Edytuj obiekt")
     button_edytuj_obiekt.grid(row=2, column=2)
@@ -77,7 +100,7 @@ def main():
     entry_lokalizacja = Entry(ramka_folmularz)
     entry_lokalizacja.grid(row=4, column=1)
 
-    button_dodaj_obiekt = Button(ramka_folmularz, text = "Dodaj obiekt")
+    button_dodaj_obiekt = Button(ramka_folmularz, text = "Dodaj obiekt", command = add_user)
     button_dodaj_obiekt.grid(row=5, column=1, columnspan=2)
 
     # ramka szczegóły obiektu - zawartość
